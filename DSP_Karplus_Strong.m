87fa3fa7c31a457c96bc3ea2f108a96fceae419c
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2020-04-27 12:46:47 -0500

    Mejoras en funciones

--- DSP_Karplus_Strong.m
+++ DSP_Karplus_Strong.m
@@ -2,42 +2,37 @@
 %Algoritmo karplus/strong
 
 %fs frecuencia de muestreo
-fs=44100;
+fs=48000;
 %FO frecuencia de la nota
 %220hz equivale a un LA
-% POO
 FO=110;
 nota(fs,FO)
 function nota = nota(fs,FO)
+%D Duracion
+D= 1;
 for n=0:11
-    Fo=FO*(2^(n/12));
-    %D Duracion
-    D= 0.5;
+    Fo=FO*(2^(n/12));   
     %Calcular el periodo de la señal 
     T =floor(fs/Fo);
     %Tañir la cuerda
-    x=3*rand(T,1)-1; %corresponde a un nivel dc de cero
+    x=(pi/8)*rand(T,1)-1; %corresponde a un nivel dc de cero
     %Numero de muestras 
     N=D*fs;
     %Crear el sonido 
-
     Y=zeros(N,1); %muestras en cero
     Y(1:T)=x;% la primer muestra igual a la entrada
+  
     pause on
-    pause(1)
+    pause(0.8)
     tono(Y,N,T,0,fs)
 end
 end
 
-function tono = tono(Y,N,T,Delay,fs)
+function tono = tono(Y,N,T,Delay,fs) 
 for i=1:N-T
     %filtro
     Y(i+T+Delay)=(Y(i)+Y(i+1))/2;
 end
 sound(Y,fs);
 plot(Y)
-
 end
-
-%plot(Y)
-
