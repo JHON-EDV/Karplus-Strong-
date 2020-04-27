3bc171595faf73b3fa04e839a06396d868469163
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2020-04-27 11:50:46 -0500

    Funciones para crear la escala de sonido y el tono

--- DSP_Karplus_Strong.m
+++ DSP_Karplus_Strong.m
@@ -5,32 +5,38 @@
 fs=44100;
 %FO frecuencia de la nota
 %220hz equivale a un LA
-FO=220;
-%D Duracion
-D= 1;
-%Calcular el periodo de la señal 
-T =floor(fs/FO);
-%Tañir la cuerda
-x=3*rand(T,1)-1; %corresponde a un nivel dc de cero
-%Numero de muestras 
-N=D*fs;
-%Crear el sonido 
+% POO
+FO=110;
+nota(fs,FO)
+function nota = nota(fs,FO)
+for n=0:11
+    Fo=FO*(2^(n/12));
+    %D Duracion
+    D= 0.5;
+    %Calcular el periodo de la señal 
+    T =floor(fs/Fo);
+    %Tañir la cuerda
+    x=3*rand(T,1)-1; %corresponde a un nivel dc de cero
+    %Numero de muestras 
+    N=D*fs;
+    %Crear el sonido 
 
-Y=zeros(N,1); %muestras en cero
-Y(1:T)=x;% la primer muestra igual a la entrada
+    Y=zeros(N,1); %muestras en cero
+    Y(1:T)=x;% la primer muestra igual a la entrada
+    pause on
+    pause(1)
+    tono(Y,N,T,0,fs)
+end
+end
 
-
-%nota(Y,N,T,Delay,fs)
-nota(Y,N,T,200,fs)
-nota(Y,N,T,10,fs)
-
-function nota= nota(Y,N,T,Delay,fs)
+function tono = tono(Y,N,T,Delay,fs)
 for i=1:N-T
     %filtro
     Y(i+T+Delay)=(Y(i)+Y(i+1))/2;
 end
 sound(Y,fs);
 plot(Y)
+
 end
 
 %plot(Y)
