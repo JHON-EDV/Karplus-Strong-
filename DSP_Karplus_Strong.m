234cd96070deec99b1f0fb2f0c5bfc198578a3dc
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2020-04-29 12:09:02 -0500

    Strings con tono y duracion

--- DSP_Karplus_Strong.m
+++ DSP_Karplus_Strong.m
@@ -1,17 +1,17 @@
 %Primer entregable DSP
 %Algoritmo karplus/strong
-
+clc;clear; close all;
 %fs frecuencia de muestreo
-fs=48100;
+%fs=181600;
 %FO frecuencia de la nota
 %110hz equivale a un LA
 FO=110; %clave de LA 
 
 %llamado de funcion loop
-loop(fs,FO)
+loop(FO)
 
 
-function loop = loop(fs,FO)
+function loop(FO)
 %Tabla Presentada en pdf 'Primera práctica de laboratorio de DSP'
 %'Conceptos Básicos de Señales y Procesamiento de Señales ' 
 
@@ -30,34 +30,45 @@ Equivalente_n = [-12 -11 -10 -9 -8 -7 -6 -5 -4 -3 ...
 Pentagrama = containers.Map(Notas_n,Equivalente_n);
 
 
-%notas={'DO','RE','MI','FA','SOL','1LA','1SI','1DO'};
-%Delay=[0,1,1,1,1,1,1,1];
-notas={'1LA','1DO','MI','SOL','1SI', ...
-    'SOL','MI','1DO','1LA',...
-    '1LA','1DO','MI','SOL','1SI', ...
-    'SOL','MI','1DO','1LA'};
-%Delay=[pi/46,pi/46,pi/46,pi/46,pi/24,...
-%    pi/46,pi/46,pi/46,pi/24,...
-%    pi/46,pi/46,pi/46,pi/46,pi/24,...
-%    pi/46,pi/46,pi/46,pi/24];
+notas = {'DO','RE','MI','FA','SOL','1LA','1SI','1DO'};
 
-Delay=pi/46;
+% notas={'1LA','1DO','MI','SOL','SI', ...
+%    'SOL','MI','1DO','1LA'...
+%    '1LA','1DO','MI','SOL','SI', ...
+%    'SOL','MI','1DO','1LA'...
+%    '1LA','1DO','MI','SOL','SI', ...
+%    'SOL','MI','1DO','1LA'...
+%    '1LA','1DO','MI','SOL','SI', ...
+%    'SOL','MI','1DO','1LA'};
+
+fs=16000;
+
+%Para la creacion de la cancion es necesario variar
+% 3 Datos -1 el arreglo de 'notas
+%         -2 el string fs (que define la duracion)
+%         -3 el string Tono_nota (que define el timbre) 
+
+%fs=[16000, 16000,16000, 16000,16000, 16000,16000, 16000];
+tono_nota=[0,0,0,0,0,0,0,0];
+
+Delay = 0.1;
 pause on
+
+%D Duracion de la nota
+D= 1;
 
 for i=1:length(notas)
     pause(Delay);
     nota_entero=cell2mat((values(Pentagrama,notas(i))));
-    nota(fs,FO,nota_entero)
+    nota(D,fs,FO,nota_entero,tono_nota)
 end
 
 end
 
 
-function nota = nota(fs,FO,n)
-%D Duracion
-D= 1;
+function nota(D,fs,FO,n,tono_nota)
 
-Fo=FO*(2^(n/12));   
+Fo=FO*(2^(n/12));
 %Calcular el periodo de la señal 
 T =floor(fs/Fo);
 %Tañir la cuerda
@@ -68,20 +79,23 @@ N=D*fs;
 Y=zeros(N,1); %muestras en cero
 Y(1:T)=x;% la primer muestra igual a la entrada
 
-tono(Y,N,T,0,fs)
+tono(Y,N,T,tono_nota,fs)
 
 end
 
 
-function tono = tono(Y,N,T,Delay,fs) 
+function tono(Y,N,T,Delay,fs) 
 for i=1:N-T
     %filtro
     Y(i+T+Delay)=(Y(i)+Y(i+1))/2;
     
 end
 sound(Y,fs);
-plot(Y)
-%t=(0:length(Y)-1)*0.0001;
+t=(0:length(Y)-1)*0.00001;
+plot(t,Y)
 
+
+%plot(Y)
+%x=length(Y)
+%z=length(t)
 end 
-
